e70ebf6e1afa30f1a47685a2f0e6ff4b1420c164
幽兰拿铁 <galaxy.nebula2021@gmail.com>
2023-06-09 01:30:32 +0800

    commit: define ULannt as Promise

--- Please ignore/index.js
+++ Please ignore/index.js
@@ -1,17 +1,55 @@
 class ULannt {
+  /**************************************** [[PromiseResult]] *****************************************/
+  #value = ""
+  
+  /**************************************** [[PromiseState]] ******************************************/
+  #state = "pending"
+  
+  
   constructor(executor /* 执行器 */) {
-    executor()
+    // 让 resolve 和 reject 中的 this 永远指向 promise 实例
+    executor(this.#resolve.bind(this), this.#reject.bind(this))
   }
   
-  resolve() {
-  
-  }
-  
-  reject() {
+  /**************************** PrivateMethod, only to be executor's param ****************************/
+  #resolve(value) {
+    if (this.#state !== "pending") return
     
+    // change [[PromiseResult]] to "fulfilled"
+    this.#value = value
+    
+    // change [[PromiseState]]
+    this.#state = "fulfilled"
+  }
+  
+  /**************************** PrivateMethod, only to be executor's param ****************************/
+  #reject(error) {
+    if (this.#state !== "pending") return
+    
+    // change [[PromiseResult]] to "rejected"
+    this.#value = error
+    
+    // change [[PromiseState]]
+    this.#state = "rejected"
+  }
+  
+  /********************************************** then ************************************************/
+  then(onFulfilled, onRejected) {
+    if (this.#state === "fulfilled") {
+      onFulfilled(this.#value)
+    }
   }
 }
 
+
 const upromise = new ULannt((resolve, reject) => {
-  console.log(123)
+  resolve("ULannt")
 })
+
+upromise.then(
+  value => {
+    console.log(value)
+  }
+)
+
+console.log(upromise)
