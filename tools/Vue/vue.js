c214a9c2dcb1b4983d379c414a2c032db65c5eb1
ULannt <galaxy.nebula2021@gmail.com>
2023-06-17 16:13:13 +0800

    commit: defined vue

--- tools/Vue/vue.js
+++ tools/Vue/vue.js
@@ -18,7 +18,7 @@ function Vue(options) {
   observe(this._data)
 }
 
-// 判断是否监听, 当数据为对象时, 监听它的属性
+/* 判断是否监听, 当数据为对象时, 监听它的属性 */
 function observe(data) {
   if (Object.prototype.toString.call(data) !== "[object Object]") return
   
@@ -26,7 +26,7 @@ function observe(data) {
   new Observer(data)
 }
 
-/******************************************* 观察者 *******************************************/
+/******************************************* 发布者 *******************************************/
 class Observer {
   constructor(data /* 经过判断, data 是对象 */) {
     for (const [k, v] of Object.entries(data)) {
@@ -48,16 +48,45 @@ class Observer {
   
   /* 数据劫持 */
   defineReactive(data, key, value) {
+    // 给每一个被劫持的数据都创建一个订阅中心, 用来收集当前数据的依赖项和通知所有依赖项进行更新
+    const dep = new Dep()
+    
     Object.defineProperty(data, key, {
       get() { /* 监听数据的获取 */
         console.log(`${ key } 被访问`)
+        
+        // 调用订阅中心的 depend 方法, 收集订阅者
+        dep.depend()
+        
         return value
       },
       set(newValue) { /* 监听数据的改变 */
         console.log(`${ key } 被修改, 修改后是 ${ newValue }`)
         value = newValue
+        
+        // 调用订阅中心的 notify 方法, 通知所有的订阅者更新
+        dep.notify()
       }
     })
+  }
+}
+
+/******************************************* 订阅中心 *******************************************/
+class Dep {
+  constructor() {
+    // 定义一个数据, 专门用来收集依赖
+    this.subs = []
+  }
+  
+  // 订阅中心任务1: 收集依赖(订阅者)
+  depend(Watcher) {
+    console.log("收集依赖(订阅者)")
+    // this.subs.push(Watcher)
+  }
+  
+  // 订阅中心任务2: 通知所有的依赖更新
+  notify() {
+    console.log("通知所有的依赖(订阅者)更新")
   }
 }
 
