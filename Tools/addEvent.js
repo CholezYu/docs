b2f802a11a6fca8184e83abf60ec65c2e978e35a
ULannt <galaxy.nebula2021@gmail.com>
2023-07-07 17:29:52 +0800

    commit

--- Tools/addEvent.js
+++
@@ -1,6 +0,0 @@
-/* 绑定二级事件, 兼容IE8 */
-export default function addEvent(element, eventname, callback) {
-  element.addEventListener
-    ? element.addEventListener(eventname, callback)
-    : element.attachEvent("on" + eventname, callback) // IE8
-}
--- Tools/getStyle.js
+++
@@ -1,6 +0,0 @@
-/* 获取元素样式, 兼容IE8 */
-export default function getStyle(element, attr) {
-  return window.getComputedStyle
-    ? getComputedStyle(element, null)[attr]
-    : element.currentStyle[attr] // IE8
-}
