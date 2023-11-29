03eedcd0b6e754066537b5c0acb156bc41cf3116
choIez <galaxy.nebula2021@gmail.com>
2023-11-29 18:39:43 +0800

    config

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress"
 import theme from "./theme.js"
 
 export default defineUserConfig({
-  base: "/",
+  base: "/docs",
   lang: "zh-CN",
   description: "vuepress-theme-hope 的文档演示",
   theme
