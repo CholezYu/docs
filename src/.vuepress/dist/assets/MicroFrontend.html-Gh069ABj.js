53db8f5fa1c578d5a8dfad6157bf9e1f8ddf0202
CholezYu <galaxy.nebula2021@gmail.com>
2024-06-23 01:30:01 +0800

    chore: build

--- src/.vuepress/dist/assets/MicroFrontend.html-Gh069ABj.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as r,c as t,b as e}from"./app-kcQb56BL.js";const c={},n=e("h2",{id:"",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#","aria-hidden":"true"},"#")],-1),a=[n];function s(_,d){return r(),t("div",null,a)}const l=o(c,[["render",s],["__file","MicroFrontend.html.vue"]]);export{l as default};
